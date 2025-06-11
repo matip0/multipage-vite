5ce3043d86e94a5d90368c26decd68ca87da65ed
matip0 <78594634+matip0@users.noreply.github.com>
2025-06-11 21:38:15 +0200

    Created login page js

--- src/login/main.js
+++ src/login/main.js
@@ -1,1 +1,17 @@
-console.log('nested');
+import {supabase} from '../supabase.js'
+
+document.getElementById('login-form').addEventListener('submit', async (e) => {
+  e.preventDefault()
+
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+
+  const {error} = await supabase.auth.signInWithPassword({email, password})
+
+  if (error) {
+    alert('Błędny email lub hasło')
+    
+  } else {
+    window.location.href = '../index.html'
+  }
+})
